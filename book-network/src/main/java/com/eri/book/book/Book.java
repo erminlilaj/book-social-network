6b96623e67cdb467381e19c391efba985c1e1474
Ermin Lilaj <ermin.lilaj04@gmail.com>
2024-08-30 14:10:11 +0200

    --using inheritance on common fields and refactor the code--

--- book-network/src/main/java/com/eri/book/book/Book.java
+++ book-network/src/main/java/com/eri/book/book/Book.java
@@ -1,6 +1,12 @@
 package com.eri.book.book;
 
+import com.eri.book.common.BaseEntity;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
@@ -8,9 +14,15 @@ import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
+
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
-@EntityListeners(AuditingEntityListener.class)
-public class Book {
+//@EntityListeners(AuditingEntityListener.class)
+public class Book extends BaseEntity {
     @Id
     @GeneratedValue
     private Integer id;
@@ -21,16 +33,16 @@ public class Book {
     private String bookCover;
     private boolean archived;
     private boolean sharable;
-    @CreatedDate
-    @Column(nullable = false, updatable = false)
-    private LocalDate createdAt;
-    @LastModifiedDate
-    @Column(insertable = false)
-    private LocalDate lastModifiedDate;
-    @CreatedBy
-    @Column(nullable = false, updatable = false)
-    private Integer createdBy;
-    @LastModifiedBy
-    @Column(insertable = false)
-    private Integer lastModifiedBy;
+//    @CreatedDate
+//    @Column(nullable = false, updatable = false)
+//    private LocalDate createdAt;
+//    @LastModifiedDate
+//    @Column(insertable = false)
+//    private LocalDate lastModifiedDate;
+//    @CreatedBy
+//    @Column(nullable = false, updatable = false)
+//    private Integer createdBy;
+//    @LastModifiedBy
+//    @Column(insertable = false)
+//    private Integer lastModifiedBy;
 }
--- book-network/src/main/java/com/eri/book/feedback/Feedback.java
+++ book-network/src/main/java/com/eri/book/feedback/Feedback.java
@@ -1,6 +1,12 @@
 package com.eri.book.feedback;
 
+import com.eri.book.common.BaseEntity;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
@@ -9,26 +15,31 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
 
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
-@EntityListeners(AuditingEntityListener.class)
-public class Feedback {
-    @Id
-    @GeneratedValue
-    private Integer id;
+//@EntityListeners(AuditingEntityListener.class)
+public class Feedback extends BaseEntity {
+//    @Id
+//    @GeneratedValue
+//    private Integer id;
     private Double note; //1-5 stars
     private String comment;
-    @CreatedDate
-    @Column(nullable = false, updatable = false)
-    private LocalDate createdAt;
-    @LastModifiedDate
-    @Column(insertable = false)
-    private LocalDate lastModifiedDate;
-    @CreatedBy
-    @Column(nullable = false, updatable = false)
-    private Integer createdBy;
-    @LastModifiedBy
-    @Column(insertable = false)
-    private Integer lastModifiedBy;
+//    @CreatedDate
+//    @Column(nullable = false, updatable = false)
+//    private LocalDate createdAt;
+//    @LastModifiedDate
+//    @Column(insertable = false)
+//    private LocalDate lastModifiedDate;
+//    @CreatedBy
+//    @Column(nullable = false, updatable = false)
+//    private Integer createdBy;
+//    @LastModifiedBy
+//    @Column(insertable = false)
+//    private Integer lastModifiedBy;
 
 
 }
