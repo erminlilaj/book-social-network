74d15117bf99ac6fd43257c262ceb0fed3b59631
Ermin Lilaj <ermin.lilaj04@gmail.com>
2024-09-03 16:43:21 +0200

    find all books by owner

--- book-network/src/main/java/com/eri/book/book/BookController.java
+++ book-network/src/main/java/com/eri/book/book/BookController.java
@@ -37,4 +37,12 @@ public class BookController {
     ){
         return ResponseEntity.ok(service.findAllBooks(page, size, connectedUser));
     }
+    @GetMapping("/owner")//get all books by owner
+    public ResponseEntity<PageResponse<BookResponse>> findAllBooksByOwner(
+            @RequestParam(name="page",defaultValue = "0",required = false) int page,
+            @RequestParam(name="page",defaultValue = "10",required = false) int size,
+            Authentication connectedUser
+    ){
+        return ResponseEntity.ok(service.findAllBooksByOwner(page, size, connectedUser));
+    }
 }
--- book-network/src/main/java/com/eri/book/book/BookService.java
+++ book-network/src/main/java/com/eri/book/book/BookService.java
@@ -51,4 +51,23 @@ public class BookService {
 
         );
     }
+
+    public PageResponse<BookResponse> findAllBooksByOwner(int page, int size, Authentication connectedUser) {
+        User user=((User) connectedUser.getPrincipal());
+        Pageable pageable= PageRequest.of(page,size, Sort.by("createdDate").descending());
+        Page<Book> books= bookRepository.findAll(BookSpecification.withOwnerId(user.getId()), pageable);
+        List<BookResponse> bookResponse= books.stream()
+                .map(bookMapper::toBookResponse)
+                .toList();
+        return new PageResponse<>(
+                bookResponse,
+                books.getNumber(),
+                books.getSize(),
+                books.getTotalElements(),
+                books.getTotalPages(),
+                books.isFirst(),
+                books.isLast()
+
+        );
+    }
 }
