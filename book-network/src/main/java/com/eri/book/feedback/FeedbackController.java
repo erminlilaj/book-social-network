4f5dd78f5e81358418c8d842eac390e2b6716284
Ermin Lilaj <ermin.lilaj04@gmail.com>
2024-09-08 19:05:59 +0200

    implement find all feedbacks

--- book-network/src/main/java/com/eri/book/feedback/FeedbackController.java
+++ book-network/src/main/java/com/eri/book/feedback/FeedbackController.java
@@ -1,15 +1,13 @@
 package com.eri.book.feedback;
 
+import com.eri.book.common.PageResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -25,5 +23,15 @@ public class FeedbackController {
             Authentication connectedUser
     ){
         return ResponseEntity.ok(service.save(request,connectedUser));
+    }
+
+    @GetMapping("/book/{book-id}")
+    public ResponseEntity<PageResponse<FeedbackResponse>> findAllFeedbackByBook(
+            @PathVariable("book-id") Integer bookId,
+            @RequestParam(name = "page",defaultValue = "0", required = false) int page,
+            @RequestParam(name = "size",defaultValue = "10", required = false) int size,
+            Authentication connectedUser
+    ){
+        return ResponseEntity.ok(service.findAllFeedbacksByBook(bookId,page,size,connectedUser));
     }
 }
--- book-network/src/main/java/com/eri/book/feedback/FeedbackMapper.java
+++ book-network/src/main/java/com/eri/book/feedback/FeedbackMapper.java
@@ -3,6 +3,8 @@ package com.eri.book.feedback;
 import com.eri.book.book.Book;
 import org.springframework.stereotype.Service;
 
+import java.util.Objects;
+
 @Service
 public class FeedbackMapper {
     public Feedback toFeedback(FeedbackRequest request) {
@@ -15,6 +17,16 @@ public class FeedbackMapper {
                         .shareable(false)//not required ;; just 4 lombok
                         .build()
                 )
+                .build();
+    }
+
+    public FeedbackResponse toFeedbackResponse(Feedback feedback, Integer id) {
+
+        return FeedbackResponse.builder()
+                .note(feedback.getNote())
+                .comment(feedback.getComment())
+                .ownFeedback(Objects.equals(feedback.getCreatedBy(), id))
+
                 .build();
     }
 }
